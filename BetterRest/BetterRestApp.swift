9fa457eebfc302e5c42c02bc719b2c173ffde569
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2021-08-04 16:06:24 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by MAC on 4/8/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
